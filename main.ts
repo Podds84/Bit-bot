4691e57f6afa5404d22e29ce29acc63f79b3a19b
Podds84 <49819638+Podds84@users.noreply.github.com>
2025-02-07 20:34:18 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,38 @@
+/**
+ * HØGRE FRAM
+ */
+/**
+ * HØGRE BAK
+ */
+/**
+ * VENSTRE FRAM
+ */
+/**
+ * VENSTRE BAK
+ */
 radio.onReceivedNumber(function (receivedNumber) {
-    let Fart = 0
     if (receivedNumber == 3) {
         if (FRAMOVER == 1) {
             bitbot.move(BBMotor.Right, BBDirection.Forward, 100)
-            bitbot.move(BBMotor.Left, BBDirection.Forward, 41)
+            bitbot.move(BBMotor.Left, BBDirection.Forward, 15)
         } else {
             bitbot.move(BBMotor.Right, BBDirection.Reverse, 100)
-            bitbot.move(BBMotor.Left, BBDirection.Reverse, 30)
+            bitbot.move(BBMotor.Left, BBDirection.Reverse, 20)
         }
     } else if (receivedNumber == 4) {
         if (FRAMOVER == 1) {
             bitbot.move(BBMotor.Left, BBDirection.Forward, 100)
-            bitbot.move(BBMotor.Right, BBDirection.Forward, 25)
+            bitbot.move(BBMotor.Right, BBDirection.Forward, 15)
         } else {
             bitbot.move(BBMotor.Left, BBDirection.Reverse, 100)
-            bitbot.move(BBMotor.Right, BBDirection.Reverse, 25)
+            bitbot.move(BBMotor.Right, BBDirection.Reverse, 10)
         }
     } else if (receivedNumber == 1) {
-        if (0 == Fart) {
-            bitbot.go(BBDirection.Forward, 60)
-            FRAMOVER = 1
-        }
+        bitbot.go(BBDirection.Forward, 50)
+        FRAMOVER = 1
     } else if (receivedNumber == 2) {
-        if (0 == Fart) {
-            bitbot.go(BBDirection.Reverse, 60)
-            FRAMOVER = 0
-        }
+        bitbot.go(BBDirection.Reverse, 60)
+        FRAMOVER = 0
     } else if (receivedNumber == 5) {
         FRAMOVER = 1
         bitbot.stop(BBStopMode.Coast)
@@ -34,10 +41,12 @@ radio.onReceivedNumber(function (receivedNumber) {
         bitbot.go(BBDirection.Forward, 100)
     } else if (receivedNumber == 7) {
         bitbot.move(BBMotor.Right, BBDirection.Forward, 100)
+        bitbot.move(BBMotor.Left, BBDirection.Forward, 0)
     } else if (receivedNumber == 8) {
         bitbot.move(BBMotor.Left, BBDirection.Forward, 100)
+        bitbot.move(BBMotor.Right, BBDirection.Forward, 0)
     }
 })
 let FRAMOVER = 0
 radio.setGroup(22)
-FRAMOVER = 0
+FRAMOVER = 1
